a4ff7a3cb04ebe0469afbaf7b9390398c8e9e484
AntonioMateus95 <antoniocmsilva95@gmail.com>
2024-04-30 00:16:18 -0300

    Modifying prompt template to return a JSON from OpenAi

--- src/main/java/guru/springframework/spring6ai/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/spring6ai/services/OpenAIServiceImpl.java
@@ -1,5 +1,8 @@
 package guru.springframework.spring6ai.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6ai.models.Answer;
 import guru.springframework.spring6ai.models.GetCapitalRequest;
 import guru.springframework.spring6ai.models.Question;
@@ -7,6 +10,7 @@ import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -28,6 +32,9 @@ public class OpenAIServiceImpl implements OpenAIService {
     @Value("classpath:templates/get_capital_with_info.st")
     private Resource getCapitalPromptWithInfo;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Override
     public String getAnswer(String question) {
         PromptTemplate promptTemplate = new PromptTemplate(question);
@@ -48,7 +55,16 @@ public class OpenAIServiceImpl implements OpenAIService {
         Prompt prompt = promptTemplate.create(Map.of("country", capitalRequest.country()));
         ChatResponse response = chatClient.call(prompt);
 
-        return new Answer(response.getResult().getOutput().getContent());
+        System.out.println(response.getResult().getOutput().getContent());
+        String responseString;
+        try {
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getContent());
+            responseString = jsonNode.get("answer").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Answer(responseString);
     }
 
     @Override
